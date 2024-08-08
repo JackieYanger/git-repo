ecd07ba0fc430967b0489fd7d7b411c605d659e3
yangsd <yangsd_jlu@163.com>
2024-08-08 16:42:57 +0800

    测试redis.修改变量名，并增加k5的值

--- src/main/java/com/practice/redis/controller/CounterController.java
+++ src/main/java/com/practice/redis/controller/CounterController.java
@@ -21,9 +21,10 @@ public class  CounterController {
         String k1 = redisTemplate.opsForValue().get("k1");
         String k2 = redisTemplate.opsForValue().get("k2");
         String k3 = redisTemplate.opsForValue().get("k3");
-        String k4 = redisTemplate.opsForValue().get("k3");
+        String k4 = redisTemplate.opsForValue().get("k4");
+        String k5 = redisTemplate.opsForValue().get("k5");
 
 
-        return String.format("有 [%d]人访问了页面, 同时取得键值为[%s]、[%s]、[%s]、[%s]", increment,k1, k2, k3, k4);
+        return String.format("有 [%d]人访问了页面, 同时取得键值为[%s]、[%s]、[%s]、[%s]、、[%s]", increment,k1, k2, k3, k4 ,k5);
     }
 }
