6d9db3a1b6ba8ab0fc2f3c31f8d08ddd61ed57a6
JackieYanger <33707108+JackieYanger@users.noreply.github.com>
2024-08-09 08:30:03 +0800

    Update RedisStudyApplication.java
    test pull

--- src/main/java/com/practice/redis/RedisStudyApplication.java
+++ src/main/java/com/practice/redis/RedisStudyApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RedisStudyApplication {
 
     public static void main(String[] args) {
+        System.out.println("test pull");
         SpringApplication.run(RedisStudyApplication.class, args);
     }
 }
